59aefaef4faded2e26222111bf58bac1e07210d4
Miguel Medina Ballesteros <miguel.medina-ballesteros@ubisoft.com>
2022-02-08 21:47:38 +0000

    Add ingame log

---
+++ Assets/Scripts/RuntimeLogGUI.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class RuntimeLogGUI : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod]
+    static void Initialize()
+    {
+        GameObject.DontDestroyOnLoad(new GameObject("Log GUI").AddComponent<RuntimeLogGUI>());
+    }
+    
+    string log;
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += Log;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= Log;
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(5, 5, Screen.width - 10, Screen.height * 0.3f - 10), log);
+    }
+
+    void Log(string logString, string stackTrace, LogType type)
+    {
+        log = logString + "\n" + log;
+    }
+}
